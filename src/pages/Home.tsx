062864763b352a8b4d5dd7faa6984b2653925419
lonelythoughts-4 <ghostgpt4@proton.me>
2026-02-09 01:13:02 +0100

    Delete src directory

--- src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-    import { useNavigate } from 'react-router-dom';
-    import Dashboard from './Dashboard';
-
-    const Home = () => {
-      return <Dashboard />;
-    };
-
-    export default Home;
